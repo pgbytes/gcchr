48f5468ddddeee85886db83f260097a295dbd641
panshul <guptapanshul@gmail.com>
2014-10-08 03:48:06 +0200

    GCCHR-4 implementing security updates- untested

--- com.gcchr.system.core/src/main/java/com/gcchr/system/core/configuration/WebSecurityConfiguration.java
+++ com.gcchr.system.core/src/main/java/com/gcchr/system/core/configuration/WebSecurityConfiguration.java
@@ -1,25 +1,41 @@
-package com.gcchr.system.common.configuration;
+package com.gcchr.system.core.configuration;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
+import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
+
+import com.gcchr.system.core.service.AuthenticationService;
 
 @Configuration
 @EnableWebMvcSecurity
 @EnableGlobalMethodSecurity
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
+    @Autowired
+    private AuthenticationService authenticationService;
+
     @Override
     protected void configure(HttpSecurity http) throws Exception
     {
-        //        http.authorizeRequests().
+        http
+                .authorizeRequests().anyRequest().authenticated();
+        http
+                .formLogin().failureUrl("/login?error")
+                .defaultSuccessUrl("/")
+                .loginPage("/login")
+                .permitAll()
+                .and()
+                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
+                .permitAll();
     }
 
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
     {
-        // auth.jdbcAuthentication().dataSource(Da)
+        auth.userDetailsService(this.authenticationService);
     }
 }
--- com.gcchr.system.core/src/main/java/com/gcchr/system/core/service/AuthenticationService.java
+++ com.gcchr.system.core/src/main/java/com/gcchr/system/core/service/AuthenticationService.java
@@ -3,13 +3,19 @@ package com.gcchr.system.core.service;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.AuthorityUtils;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 import com.gcchr.system.dal.model.UserAccount;
 import com.gcchr.system.dal.service.UserAccountService;
 
 @Service
-public class AuthenticationService
+public class AuthenticationService implements UserDetailsService
 {
     private final UserAccountService userAccountService;
 
@@ -23,4 +29,21 @@ public class AuthenticationService
     {
         return this.userAccountService.fetchAllLogins();
     }
+
+    public UserDetails loadUserByUsername(final String username) throws UsernameNotFoundException
+    {
+        UserAccount userAccount = this.userAccountService.findByUsername(username);
+
+        if (userAccount == null)
+        {
+            return null;
+        }
+
+        List<GrantedAuthority> auth = AuthorityUtils.commaSeparatedStringToAuthorityList(userAccount.getUserRoles().toString());
+
+        String password = userAccount.getPassword();
+
+        return new User(username, password, auth);
+    }
+
 }
