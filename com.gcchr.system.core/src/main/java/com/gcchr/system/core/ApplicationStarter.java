fbf0630b8dd033c78e4fe7758769bc3b574a27da
panshul <guptapanshul@gmail.com>
2014-10-09 03:20:39 +0200

    GCCHR-4 implementing security updates- working with basic auth reading user info from MongoDB

--- com.gcchr.system.core/src/main/java/com/gcchr/system/core/ApplicationStarter.java
+++ com.gcchr.system.core/src/main/java/com/gcchr/system/core/ApplicationStarter.java
@@ -1,23 +1,20 @@
 package com.gcchr.system.core;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.PropertySource;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 @ComponentScan("com.gcchr.system")
 @EnableAutoConfiguration
 @PropertySource("classpath:/application.properties")
-public class ApplicationStarter implements CommandLineRunner
+public class ApplicationStarter extends WebMvcConfigurerAdapter
 {
     public static void main(String[] args)
     {
         SpringApplication.run(ApplicationStarter.class, args);
     }
 
-    @Override public void run(String... strings) throws Exception
-    {
 
-    }
 }
--- com.gcchr.system.core/src/main/java/com/gcchr/system/core/configuration/WebSecurityConfiguration.java
+++ com.gcchr.system.core/src/main/java/com/gcchr/system/core/configuration/WebSecurityConfiguration.java
@@ -1,41 +1,40 @@
 package com.gcchr.system.core.configuration;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.security.SecurityProperties;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
-import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
-import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.config.annotation.web.servlet.configuration.EnableWebMvcSecurity;
-import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 import com.gcchr.system.core.service.AuthenticationService;
 
 @Configuration
 @EnableWebMvcSecurity
-@EnableGlobalMethodSecurity
+//@EnableGlobalMethodSecurity
+@Order(SecurityProperties.ACCESS_OVERRIDE_ORDER)
 public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter
 {
     @Autowired
+    private SecurityProperties security;
+
+    @Autowired
     private AuthenticationService authenticationService;
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception
-    {
-        http
-                .authorizeRequests().anyRequest().authenticated();
-        http
-                .formLogin().failureUrl("/login?error")
-                .defaultSuccessUrl("/")
-                .loginPage("/login")
-                .permitAll()
-                .and()
-                .logout().logoutRequestMatcher(new AntPathRequestMatcher("/logout")).logoutSuccessUrl("/login")
-                .permitAll();
-    }
+    //    //@Override
+    //    protected void configure(HttpSecurity http) throws Exception
+    //    {
+    //        http.authorizeRequests().anyRequest().authenticated().and().httpBasic();
+    //    }
 
-    public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception
+    @Override
+    public void configure(AuthenticationManagerBuilder auth) throws Exception
     {
         auth.userDetailsService(this.authenticationService);
+
+        //        auth.inMemoryAuthentication().withUser("admin").password("admin")
+        //                .roles("ADMIN", "USER").and().withUser("user").password("user")
+        //                .roles("USER");
     }
 }
